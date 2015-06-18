0d090c2d362c90727acac7bc800d7d9e7a89712b
Jurica Vukadin <u.ra604@gmail.com>
2015-06-18 20:05:52 +0200

    mt2: Update Adreno libraries
    * Package version: LA.BF..1.1.1.C2.05.00.00.046

--- mt2/mt2-vendor-blobs.mk
+++ mt2/mt2-vendor-blobs.mk
@@ -358,11 +358,10 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/mt2/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
-    vendor/huawei/mt2/proprietary/vendor/lib/libOpenVG.so:system/vendor/lib/libOpenVG.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+    vendor/huawei/mt2/proprietary/vendor/lib/libbccQTI.so:system/vendor/lib/libbccQTI.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
-    vendor/huawei/mt2/proprietary/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
     vendor/huawei/mt2/proprietary/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
