23e459feeda2ad4f2e386d93a84815356dc9d3c9
Jurica Vukadin <u.ra604@gmail.com>
2015-07-08 11:28:06 +0200

    mt2: Remove vendor wcnss_service

--- mt2/mt2-vendor-blobs.mk
+++ mt2/mt2-vendor-blobs.mk
@@ -295,5 +295,4 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/mt2/proprietary/etc/firmware/venus.b03:system/etc/firmware/venus.b03 \
     vendor/huawei/mt2/proprietary/etc/firmware/venus.b04:system/etc/firmware/venus.b04 \
     vendor/huawei/mt2/proprietary/etc/firmware/venus.mbn:system/etc/firmware/venus.mbn \
-    vendor/huawei/mt2/proprietary/etc/firmware/venus.mdt:system/etc/firmware/venus.mdt \
-    vendor/huawei/mt2/proprietary/bin/wcnss_service:system/bin/wcnss_service
+    vendor/huawei/mt2/proprietary/etc/firmware/venus.mdt:system/etc/firmware/venus.mdt
