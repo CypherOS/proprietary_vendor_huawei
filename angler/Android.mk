8d65ed4060e29cb194a438a9a940e1ffe309828e
razorloves <razorloves@gmail.com>
2016-08-29 22:53:16 -0500

    angler: flag app that is in playstore as presigned
    This prevents signature check failure and certificate mismatch error from the playstore.
    
    Change-Id: Id0b2f46f837a5c061b9ac0192291e0358b959ce3

--- angler/Android.mk
+++ angler/Android.mk
@@ -53,7 +53,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := HotwordEnrollment
 LOCAL_MODULE_OWNER := huawei
 LOCAL_SRC_FILES := proprietary/priv-app/HotwordEnrollment/HotwordEnrollment.apk
-LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := .apk
