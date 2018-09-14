39c4d489867641d18f495086b9635f6302ed98ca
Artem Borisov <dedsa2002@gmail.com>
2018-09-14 21:50:10 +0300

    kirin970-common: Add missing hostapd_hisi dependencies
    Change-Id: Ib4a49e621b0d113798e42d943b8497cede9bd329

--- kirin970-common/kirin970-common-vendor.mk
+++ kirin970-common/kirin970-common-vendor.mk
@@ -391,5 +391,8 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/kirin970-common/proprietary/lib64/vndk-27/libicui18n.so:system/lib64/vndk-27/libicui18n.so \
     vendor/huawei/kirin970-common/proprietary/lib64/vndk-27/libicuuc.so:system/lib64/vndk-27/libicuuc.so \
     vendor/huawei/kirin970-common/proprietary/lib64/vndk-27/libneuralnetworks.so:system/lib64/vndk-27/libneuralnetworks.so \
+	vendor/huawei/kirin970-common/proprietary/lib64/libchrlog.so:system/lib64/libchrlog.so \
+    vendor/huawei/kirin970-common/proprietary/lib64/libc_sec_hisi_connectivity.so:system/lib64/libc_sec_hisi_connectivity.so \
+    vendor/huawei/kirin970-common/proprietary/lib64/libpf_customize_hisi.so:system/lib64/libpf_customize_hisi.so \
     vendor/huawei/kirin970-common/proprietary/lib64/libwifi-system.so:system/lib64/libwifi-system.so \
 	vendor/huawei/kirin970-common/proprietary/lib64/libwifi-hal.so:system/lib64/libwifi-hal.so
